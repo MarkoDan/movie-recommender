be4dc40e8d080edcf408345bd6286de4676e7616
Marko Dankovich <dankovic.marko@yahoo.com>
2023-09-04 22:18:21 +0200

    Authentication

--- movierecommender/urls.py
+++ movierecommender/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'movie_recommender'
 
 urlpatterns = [
     # route is a string contains a URL pattern
     path(route='', view=views.movie_recommendation_view, name='recommendations'),
+    path('logout/', views.logout_request, name='logout'),
+    path('login/', views.login_request, name='login'),
+    path('registration/', views.registration_request, name='registration'),
 
 ]
--- movierecommender/views.py
+++ movierecommender/views.py
@@ -1,9 +1,15 @@
+from venv import logger
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from . import views
 from .models import Movie
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.models import User
+import logging
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
-# HINT: Create a view to provide movie recommendations list for the HTML template
 
 def movie_recommendation_view(request):
     if request.method == "POST":
@@ -51,7 +57,67 @@ def generate_movies_context():
     context['movie_list'] = movies
 
     return context
+
+def login_request(request):
+    context = {}
+    #Handles post request
+    if request.method == "POST":
+        #Get username and password from request.POST dictionary
+        username = request.POST['username']
+        password = request.POST['psw']
+    
+        #Try to check if provided credential can be authenticated
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            #If user is valid, call login method and login current user
+            login(request, user)
+            return redirect('movie_recommender:recommendations')
+        else:
+            return render(request, 'movierecommender/user_login.html', context)
+    else:
+        return render(request, 'movierecommender/user_login.html', context)
+
+
+def registration_request(request):
+    context = {}
+
+    if request.method == "GET":
+        return render(request, 'movierecommender/user_registration.html', context)
+
+    elif request.method == "POST":
+        # Get user information from request.POST
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        username = request.POST['username']
+        password = request.POST['psw']
         
+        user_exists = False
+        try:
+            # Check if user already exists
+            User.objects.get(username=username)
+            user_exists = True
+        except:
+            # If not, simply log this is a new user
+            logger.debug("{} is new user", format(username))
+
+        if not user_exists:
+            #Create user in auth_user table
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+
+            login(request, user)
+            return redirect('movie_recommender:recommendations')
+        else:
+            return render(request, 'movierecommender/user_registration.html', context)
+
+
+def logout_request(request):
+    #Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+
+    #Logout user in the request
+    logout(request)
+    #Redirect user back to course list view
+    return redirect('movie_recommender:recommendations')
 
 # def get_movie_recommendations(request):
 #     start = int(request.GET.get('start', 0))
